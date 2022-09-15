c07b3fc2073bdccad4dd0a910734d7f4374d8f52
davidr3489@gmail.com <crowden3489>
2022-09-15 22:02:41 +0200

    Finished Version

--- main.py
+++ main.py
@@ -1,80 +1,102 @@
-from text import text_list, sample_text
+import tkinter
+
+from text import text_list
 from tkinter import *
 
-# user_text = input('Please type the following paragraph').split(' ')
+FONT = 'Courier'
+BACKGROUND = '#7FB77E'
+FONT_COLOR = '#F7F6DC'
+MINUTE = 60
+
 window = Tk()
+window.title('Typing Speed Test')
+window.config(padx=30, pady=30, bg=BACKGROUND, height=2000, width=650)
 
-# def calculate_wpm():
-#     global user_text
-#     WPM = 0
-#     errors = 0
-#     for words in user_text:
-#         if words in text_list:
-#             WPM += 1
-#         elif words not in text_list:
-#             errors += 1
+sample_text = (" ".join(text_list))
 
-better_sample = (" ".join(text_list))
+title = Label(window, text='Test How Fast You Can Type!', font=(FONT, 25, 'bold'), bg=BACKGROUND, pady=10)
+title.grid(column=0, row=0, columnspan=2)
 
-text_window = Text(window, height=10, width=99, wrap=WORD)
-text_window.insert(INSERT, better_sample)
+subtitle_1 = Label(window, text="Click the Start Button When You're Ready", font=(FONT, 20, 'bold'), bg=BACKGROUND,
+                   foreground=FONT_COLOR, pady=10)
+subtitle_1.grid(row=1, column=0, columnspan=2)
+subtitle_2 = Label(window, text=" You Have 60 seconds to Type as Much of the Text as You Can", font=(FONT, 20, 'bold'),
+                   bg=BACKGROUND, pady=10)
+subtitle_2.grid(row=2, column=0, columnspan=2)
+text_window = Text(window, height=12, width=60, wrap=WORD, pady=20, bg=BACKGROUND, highlightthickness=0, borderwidth=0, font=(FONT,10, 'bold'))
+text_window.insert(INSERT, sample_text)
 text_window.configure(state=DISABLED)
-text_window.pack()
+text_window.grid(row=3, column=1, rowspan=4)
 
-# countVar = StringVar()
-# pos = text_window.search("calculates", "1.0", stopindex="end", count=countVar)
-# end_point = pos + f"+{countVar.get()}c"
-# text_window.tag_configure("search", background="green")
-# text_window.tag_add("search", pos, end_point)
-
-type_input = Entry(window, width=99)
-type_input.pack()
-
-game_on = False
-type_list = []
+type_input = Entry(window, width=30, font=(FONT, 15))
+type_input.insert(END, 'Write Text Here')
+type_input.grid(column=0, row=4)
 
 
-# def start_game():
-#     global game_on
-#     game_on = True
-#     while game_on:
-#         user_type = type_input.get()
-#         type_list.append(user_type.split())
-
-
-errors = []
 def submit():
-    global text_list
+    type_input.delete(0, tkinter.END)
+    type_input.focus_set()
+    start_game()
+    countdown(MINUTE)
+    window.after(1000 * MINUTE, end_game)
+
+
+def start_game():
     user_type = type_input.get()
-    type_list.append(user_type.split())
-    for words in type_list:
-        try:
+    users_input = user_type.split(' ')
+    for words in users_input:
+        if words in text_list:
             countVar = StringVar()
             pos = text_window.search(words, "1.0", stopindex="end", count=countVar)
             end_point = pos + f"+{countVar.get()}c"
             text_window.tag_configure("search", background="green")
             text_window.tag_add("search", pos, end_point)
-        except:
-            errors.append(words)
+        else:
             continue
+    window.after(500, start_game)
 
 
+def countdown(time_left):
+    countdown_clock['text'] = time_left
+    if time_left > 0:
+        window.after(1000, countdown, time_left - 1)
+    if time_left < 10:
+        countdown_clock.config(foreground='red')
+        countdown_clock['text'] = f'0{time_left}'
+    if time_left < 1:
+        countdown_clock['text'] = "Time's up!"
 
-# start_button = Button(window, text='Start Game', command=start_game)
-# start_button.pack()
 
-submit_button = Button(window, text='Submit', command=submit)
-submit_button.pack()
+def end_game():
+    type_input.configure(state=DISABLED)
+    user_type = type_input.get()
+    users_input = user_type.split(' ')
+    WPM = 0
+    errors = 0
+    for words in users_input:
+        if words in text_list:
+            WPM += 1
+        else:
+            errors += 1
+    show_results(WPM, errors)
 
-# print(f'Your words-per-minute = {WPM}')
-# print(f'Number of errors = {errors}')
 
-# implement timing device from looking at pomadoro technique code
-# another for loop to cover the words as they're typed and matched.
-# after the timer is over, use python to hit the enter button
-# create a while game on loop that's constantly 'getting' the text being inputted into the text box, and every time it
-# matches it creates a new number in wpm
-# when the match happens highlight the word in green and maybe delete?
-# use the timer as the game_on boolean
+def show_results(result, mistakes):
+    final_wpm = Label(window, text=f"Typing Speed: {result} Words per Minute", background=BACKGROUND,
+                      font=(FONT, 15, 'bold'), foreground=FONT_COLOR, pady=30)
+    if result <= 1:
+        final_wpm.config(text=f'Typing Speed: {result} Word per Minute')
+    final_wpm.grid(column=0, row=7)
+    final_errors = Label(window, text=f'Total Errors Made: {mistakes}', background=BACKGROUND, font=(FONT, 15, 'bold'),
+                         foreground=FONT_COLOR)
+    final_errors.grid(column=1, row=7)
+
+
+start_button = Button(window, text='Start Game', command=submit)
+start_button.grid(column=0, row=5)
+
+countdown_clock = Label(window, bg=BACKGROUND, anchor='center', foreground=FONT_COLOR, font=(FONT, 40, 'bold'))
+countdown_clock.grid(column=0, row=6)
 
 window.mainloop()
+
--- text.py
+++ text.py
@@ -1,10 +1,10 @@
-sample_text = "one hundred years ago she went to the shop to buy a packet of crisps and drink but honestly he " \
-              "wasn't especially huge fan fizzy beverages so took flight whilst reading pretty horrible book about monsters " \
-              "plus other things that frankly I don't remember because text is total rubbish additionally really unsure " \
-              "know what one be talking upon so just trust me that could something useful plot that when you done " \
-              "writing this complete rubbish you will have a sample text that you will be able to use for your next " \
-              "python project which is to create a programme which calculates your words per minute speed based on " \
-              "how much crap you're writing her on this code so let's see how many words you're able to type in a " \
-              "minute you lazy lazy person best of luck and I hope you have the most pleasant day "
+sample_text = "one two hundred years ago she went lap head to sun moon keep the shop buy packet of crisps and drink drank but honestly alien " \
+              "wasn't tool huge fan lift waft fizzy car bus so took fly tin tug push whilst reading pretty horrible book about monsters " \
+              "plus other things that frankly I don't remember because text total rubbish additionally really unsure " \
+              "know what humans talking upon doe just trust could something useful plot behind when you done " \
+              "writing this complete rubbish you will have a sample able incapable for your next " \
+              "python project which create programme calculates words per minute speed based " \
+              "how much crap you're code shake let's see many type " \
+              "lazy person best luck royalty hope most pleasant day "
 
 text_list = sample_text.split(' ')
